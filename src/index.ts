1b588f4d9512fa5e2f3dbdf2fb4e1ed1db51828c
themilho <themilhodev@gmail.com>
2025-08-03 20:18:24 -0300

    cors library configured to communicate with the frontend

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import {createServer} from 'http';
+import cors from 'cors';
 import errorHandler  from './middlewares/errorHandler';
 import router from './routes/indexRoutes';
 
@@ -10,6 +11,7 @@ const app = express();
 const server = createServer(app);
 
 
+app.use(cors());
 app.use(express.json());
 app.use('/', router);
 app.use(errorHandler);
