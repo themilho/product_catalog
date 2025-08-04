79c2943650da409e5fd8cd3d88b363e806f41d14
themilho <themilhodev@gmail.com>
2025-08-04 20:06:40 -0300

    the location of the index.ts folder has been changed to within apifolder

---
+++ api/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from "dotenv";
+import { createServer } from 'http';
+import cors from 'cors';
+import errorHandler from '../src/middlewares/errorHandler';
+import router from '../src/routes/indexRoutes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
+app.use(errorHandler);
+
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  const server = createServer(app);
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
+// Export the Express API for Vercel
+export default app;
